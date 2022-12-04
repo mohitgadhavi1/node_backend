f9af42118faeb635fad45e7f0e453f39059b55f5
Mohit Gadhavi <mohit.gadhavi@zybisys.com>
2022-12-04 21:50:49 +0530

    conected to DB

--- server/httpServer.js
+++
@@ -1,7 +0,0 @@
-const http = require("http");
-const server = http.createServer((req, res) =>
-  res.end("Hello from the other side.")
-);
-server.listen(8000, "127.0.0.1", () => {
-  console.log("listening");
-});
